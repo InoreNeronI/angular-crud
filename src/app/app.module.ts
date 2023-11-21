0399fd008d9ed8ee8aebff4d1751c79c046a034f
mmozos <martinmozos@gmail.com>
2023-11-21 10:52:56 +0100

    Upgrade to latest and greatest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,11 +17,11 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    MatTableModule,
     MatButtonModule,
-    MatIconModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
+    MatTableModule,
     BrowserAnimationsModule,
     MatCardModule
   ],
